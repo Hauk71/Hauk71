c135000a063834890c62fe7eea59e0b838c0a5ac
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-09-10 11:10:58 -0400

    Wk 1 graded

--- code/Hauk_Week1_Assignment.R
+++ code/Hauk_Week1_Assignment.R
@@ -38,14 +38,14 @@ W <- c('A','B','C','D','L','F','G','H','I','e','J','t','K','z','q')
 V <- c('w','a','w','a','a','w','b','w','b','w','b','b','a','b','a')
 
 # binds the data to a single data frame
-data <- cbind(V,W,X,Y,Z,ZZ)
+data <- cbind(V,W,X,Y,Z,ZZ) #What is ZZ?? this is not in your script and it breaks the rest of your code.
 data
 # reconfigure to allow numeric values
 df <- as.data.frame(data)
 df
 
 #renaming column names
-colnames(df) <- c("one","two","three","four","five",'six')
+colnames(df) <- c("one","two","three","four","five",'six') #Too many names - make sure you are paying attention to the errors in the console.
 df
 
 #renaming the row
@@ -53,19 +53,19 @@ row.names(df) <- df$two
 df
 
 #deleting a column
-df[,-2]
+df[,-2]#this only prints the data frame without that column, it does not create a new object.
 
 #add one row
-ZZ <- c(53,765,8552,852,95,71,83,64,5810,2,56,25,12,76,32)
+ZZ <- c(53,765,8552,852,95,71,83,64,5810,2,56,25,12,76,32) #this is a column
 #repeat by adding into the data from above
 
 ####summary of the data
-summary(df)
+summary(df) #summary should have numeric values for the numeric columns.
 
 #export data frame to csv file
-df <- data.frame('one' = c('w','a','w','a','a','w','b','w','b','w','b','b','a','b','a'), 'three' = c(4,8,9,7,10,53,11,67,74,72,74567,456,5,1,14), 'four' = c(36,42,57,77,24,56,77,33,43,47,78,12,45,36,1), 'five' = c(36777,4848,3862,192.34,387.34,37.45,267.5,23,245,34,78,15,3,66,24), 'six' = c(53,765,8552,852,95,71,83,64,5810,2,56,25,12,76,32))
+df <- data.frame('one' = c('w','a','w','a','a','w','b','w','b','w','b','b','a','b','a'), 'three' = c(4,8,9,7,10,53,11,67,74,72,74567,456,5,1,14), 'four' = c(36,42,57,77,24,56,77,33,43,47,78,12,45,36,1), 'five' = c(36777,4848,3862,192.34,387.34,37.45,267.5,23,245,34,78,15,3,66,24), 'six' = c(53,765,8552,852,95,71,83,64,5810,2,56,25,12,76,32)) #what is this? Not how you shold be doing this.
 print(df)
-write.csv(df,C:\\Users\\Documents\\GitHub\\R4Eco_2022\\week1)
+write.csv(df,C:\\Users\\Documents\\GitHub\\R4Eco_2022\\week1) #you need quotes around the file path, also wrong location.
 
 
 # one                two               three               four               five               six           
