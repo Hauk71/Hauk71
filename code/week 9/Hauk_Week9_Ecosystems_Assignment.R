f8ec86a4f52406078822ab5637355e6f40e0fbe8
Matt Wilson <wilsonmatt@susqu.edu>
2022-11-04 12:00:48 -0400

    Wk9 graded

--- code/week 9/Hauk_Week9_Ecosystems_Assignment.R
+++ code/week 9/Hauk_Week9_Ecosystems_Assignment.R
@@ -54,6 +54,7 @@ ord <- rda(Iv.mean2 ~ totalN, AB.mean2)
 anova(ord)
 plot(ord, ylim = c(-2,2), xlim = c(-5,5))
 #This data shows that there is a significant difference between the variance of the model vs the residual. The plot shows that the toatlN is a limiting factor in this environment due to the lack of support from the data points. 
+#I do not get a significant result when running this code. Even if significant, you need to think about what the result tells you mode - this just demonstrates that the communities are related to nitrogen - some communities are a result of more N, some from less N.
 
 #Q2
 plants.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Data_intro_exp_4_species")
@@ -79,12 +80,15 @@ plot(mod2$residuals)
 
 #The data does not change to much due to the fact that there are data points that outweigh others or have a greater impact on the environment making the removal of these data points insignificant. 
 #this can be seen with the removal of calcium and aluminum, where the total N is much more important and takes over most of the data points. This can also be seen in the environment because nitrogen is a limiting reagent in plants and other autographs, making it significantly more important the the uptake of Al.
+#Where does uptake of Al fit into your results? I'm confused. I think I know what you are saying here, but your terminology is confusing.
+#Data never change during an analysis, only the results/models change from changing data that are a part of the model. This is also the same data I used in the tutorial...
 
 #Q3
 #These results relate to one another by emphasizing the significant and insignificant data. The data shows that the removal of certain smaller points played no major role or affect in the final output of the linear model. 
 #The abiotic and biotic interactions throughout life are important even at the smallest levels, With some of these data points affecting abiotic factors, some neutrally like calcium, and others more significant like nitrogen intake, we are able to see how they ultimately impact biotic aspects of this world.
 #Most abiotic factors have an impact on biotic for example plants need nitrogen to grow and the abiotic factors in the atmosphere help the nitrogen flow to be intake by the plants. 
+#Nitrogen cycle does not work by direct uptake of N by plants - need bacteria for that to happen.
 
-
+#non-significant, not insignificant.
 
 
