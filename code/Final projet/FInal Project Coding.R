accd09ad7a00499d82f638bfb3178f26aae9a8ed
Hauk71 <Hauk@susqu.edu>
2022-12-06 09:48:36 -0500

    Final pt.2

--- code/Final projet/FInal Project Coding.R
+++ code/Final projet/FInal Project Coding.R
@@ -10,10 +10,12 @@ setwd('C:/GitHub/Hauk71/code/Final projet')
 Shell <- read.csv("C:/GitHub/Hauk71/code/Final projet/Macoma balthic shell lengths.csv")
 Survival <- read.csv("C:/GitHub/Hauk71/code/Final projet/Macoma balthica survival.csv")
 Recruits <- read.csv("C:/GitHub/Hauk71/code/Final projet/Macoma balthic recruits.csv")
+LandM <- read.csv("C:/GitHub/Hauk71/code/Final projet/Macoma balthic lengths and masses.csv")
 
 summary(Shell)
 summary(Survival)
 summary(Recruits)
+summary(LandM)
 
 #data from hypoxia tolerance 
 Fireworm <- read.csv("C:/GitHub/Hauk71/code/Final projet/fireworm_experiment.csv")
@@ -21,4 +23,33 @@ Physical <- read.csv("C:/GitHub/Hauk71/code/Final projet/physical_data.csv")
 
 summary(Fireworm)
 summary(Physical)
+
+#aggregate
+Shelldata <- as.data.frame(Shell)
+Survivaldata <- as.data.frame(Survival)
+Recruitsdata <- as.data.frame(Recruits)
+Firewormdata <- as.data.frame(Fireworm)
+Physicaldata <- as.data.frame(Physical)
+LandMdata <- as.data.frame(LandM)
+
+
+#combining objects
+
+
+
+
+#naming
+LandMName <- paste(LandMdata$Shell.length..mm, LandMdata$Average.DO..mg.L.)
+LandM$Name <- LandMName
+
+ShellName <- paste(Shelldata$Shell.length..mm, Shelldata$Average.DO..mg.L.)
+Shell$Name <- ShellName
+
+ShellMeans <- aggregate(x =Shelldata, by = list(Shell$Name), FUN = 'mean')
+LandMMeans <- aggregate(x =LandMdata, by = list(LandM$Name), FUN = 'mean')
+
+colnames(SurvivalMeans)
+ord <- lm(ShellMeans ~ Survival, LandMMeans)
+
+
 
